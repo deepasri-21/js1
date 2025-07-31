3f3e98aabf5c58ec5f770a4bacea7118e92947f9
deepasri-21 <144312010+deepasri-21@users.noreply.github.com>
2025-07-31 23:04:05 +0530

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -2,5 +2,5 @@ let Name="Deepasri";
 let age=21;
 let rollno=2104;
 let job="Full stack developer";
-console.log(Name);
-console.log(Name,age,rollno,job)
+console.log(Name);  //Deepasri
+console.log(Name,age,rollno,job);  //Deepasri,21,2104,Full Stack developer
